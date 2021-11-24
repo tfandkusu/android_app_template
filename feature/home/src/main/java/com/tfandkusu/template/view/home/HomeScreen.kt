b6aba08f779207e24b1b6eb5fc4417cbd5545162
Haruhiko Takada <haruhiko.takada@gmail.com>
2021-11-25 04:45:42 +0900

    Make temporal HomeScreen

--- feature/home/src/main/java/com/tfandkusu/template/view/home/HomeScreen.kt
+++ feature/home/src/main/java/com/tfandkusu/template/view/home/HomeScreen.kt
@@ -1,12 +1,25 @@
 package com.tfandkusu.template.view.home
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.MutableLiveData
+import com.tfandkusu.template.home.R
+import com.tfandkusu.template.viewmodel.home.HomeEffect
 import com.tfandkusu.template.viewmodel.home.HomeEvent
+import com.tfandkusu.template.viewmodel.home.HomeState
 import com.tfandkusu.template.viewmodel.home.HomeViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel) {
@@ -14,13 +27,32 @@ fun HomeScreen(viewModel: HomeViewModel) {
         viewModel.event(HomeEvent.OnCreate)
         viewModel.event(HomeEvent.Load)
     }
-    // A surface container using the 'background' color from the theme
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text(stringResource(R.string.app_name))
+            })
+        }
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview
+fun HomeScreenPreview() {
+    HomeScreen(object : HomeViewModel {
+        override val state = MutableLiveData(HomeState())
+
+        override val effect: Flow<HomeEffect>
+            get() = flow {}
+
+        override fun event(event: HomeEvent) {
+        }
+    })
 }
