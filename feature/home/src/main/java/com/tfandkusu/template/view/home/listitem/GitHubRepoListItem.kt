2a91b17ae9a1a0e1f78165e8e1024c03824e33d2
Haruhiko Takada <haruhiko.takada@gmail.com>
2021-12-20 00:09:06 +0900

    Call web page

--- feature/home/src/main/java/com/tfandkusu/template/view/home/listitem/GitHubRepoListItem.kt
+++ feature/home/src/main/java/com/tfandkusu/template/view/home/listitem/GitHubRepoListItem.kt
@@ -1,6 +1,9 @@
 package com.tfandkusu.template.view.home.listitem
 
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.integerArrayResource
 import androidx.compose.ui.res.stringArrayResource
@@ -32,7 +36,14 @@ import java.util.Date
 
 @Composable
 fun GitHubRepoListItem(repo: GithubRepo) {
-    Column {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.clickable {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(repo.htmlUrl)
+            context.startActivity(intent)
+        }
+    ) {
         Spacer(modifier = Modifier.height(16.dp))
         Row(
             verticalAlignment = Alignment.CenterVertically,
