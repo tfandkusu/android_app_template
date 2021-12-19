7a330d51ce93c65c7aa31c7d4808f377653e08ec
Haruhiko Takada <haruhiko.takada@gmail.com>
2021-12-20 03:10:52 +0900

    Use catalog for compose preview.

--- feature/home/src/main/java/com/tfandkusu/template/view/home/HomeScreen.kt
+++ feature/home/src/main/java/com/tfandkusu/template/view/home/HomeScreen.kt
@@ -15,9 +15,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.tfandkusu.template.catalog.GitHubRepoCatalog
 import com.tfandkusu.template.home.R
-import com.tfandkusu.template.model.GithubRepo
-import com.tfandkusu.template.util.parseUTC
 import com.tfandkusu.template.view.home.listitem.GitHubRepoListItem
 import com.tfandkusu.template.viewmodel.home.HomeEffect
 import com.tfandkusu.template.viewmodel.home.HomeEvent
@@ -82,37 +81,7 @@ fun HomeScreenPreviewProgress() {
 @Composable
 @Preview
 fun HomeScreenPreviewList() {
-    val repo1 = GithubRepo(
-        229475311L,
-        "observe_room",
-        listOf(
-            "Check how to use Room to observe SQLite database ",
-            "and reflect the changes in the RecyclerView."
-        ).joinToString(separator = ""),
-        parseUTC("2021-10-29T00:15:46Z"),
-        "Kotlin",
-        "https://github.com/tfandkusu/observe_room",
-        false
-    )
-    val repo2 = GithubRepo(
-        320900929L,
-        "groupie_sticky_header_sample",
-        "Sample app for sticky header on the groupie",
-        parseUTC("2021-01-19T19:46:27Z"),
-        "Java",
-        "https://github.com/tfandkusu/groupie_sticky_header_sample",
-        false
-    )
-    val repo3 = GithubRepo(
-        343133709L,
-        "conference-app-2021",
-        "The Official App for DroidKaigi 2021",
-        parseUTC("2021-09-21T16:56:04Z"),
-        "Kotlin",
-        "https://github.com/tfandkusu/conference-app-2021",
-        true
-    )
-    val repos = listOf(repo1, repo2, repo3)
+    val repos = GitHubRepoCatalog.getList()
     val state = HomeState(
         progress = false,
         repos = repos
