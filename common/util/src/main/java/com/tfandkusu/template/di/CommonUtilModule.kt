bc35635dc0b0ffa40d4628a846238b3a23dba261
Haruhiko Takada <haruhiko.takada@gmail.com>
2021-12-25 21:54:35 +0900

    Rename Dagger Hilt module.

--- common/util/src/main/java/com/tfandkusu/template/di/CommonUtilModule.kt
+++ common/util/src/main/java/com/tfandkusu/template/di/CommonUtilModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class CommonModule {
+abstract class CommonUtilModule {
     @Binds
     @Singleton
     abstract fun bindCurrentTimeGetter(currentTimeGetter: CurrentTimeGetterImpl): CurrentTimeGetter
