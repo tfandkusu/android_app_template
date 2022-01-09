80bd9f387653d72afa6ce54022dc20a9f6e08601
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-01-10 01:57:58 +0900

    Show OSS License Activity

--- feature/info/view/src/main/java/com/tfandkusu/template/view/info/InfoFragment.kt
+++ feature/info/view/src/main/java/com/tfandkusu/template/view/info/InfoFragment.kt
@@ -1,11 +1,31 @@
 package com.tfandkusu.template.view.info
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import com.tfandkusu.template.info.view.R
 
 class InfoFragment : PreferenceFragmentCompat() {
+    companion object {
+        private const val KEY_OSS = "oss"
+
+        private const val KEY_ABOUT = "about"
+    }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preference_info, rootKey)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        findPreference<Preference>(KEY_OSS)?.setOnPreferenceClickListener {
+            OssLicensesMenuActivity.setActivityTitle(getString(R.string.title_oss_license))
+            val intent = Intent(requireContext(), OssLicensesMenuActivity::class.java)
+            startActivity(intent)
+            true
+        }
     }
 }
