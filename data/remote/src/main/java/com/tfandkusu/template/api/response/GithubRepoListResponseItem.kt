8bb1846f34d0cf0f62ee656fea70a6be0133a5d0
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-05-22 05:28:08 +0900

    Setup shrinking, obfuscation, and optimization to release build

--- data/remote/src/main/java/com/tfandkusu/template/api/response/GithubRepoListResponseItem.kt
+++ data/remote/src/main/java/com/tfandkusu/template/api/response/GithubRepoListResponseItem.kt
@@ -1,8 +1,10 @@
 package com.tfandkusu.template.api.response
 
+import androidx.annotation.Keep
 import com.squareup.moshi.Json
 import java.util.Date
 
+@Keep
 data class GithubRepoListResponseItem(
     val id: Long,
     val name: String,
--- viewCommon/src/main/java/com/tfandkusu/template/view/info/InfoActivityAlias.kt
+++ viewCommon/src/main/java/com/tfandkusu/template/view/info/InfoActivityAlias.kt
@@ -1,3 +1,6 @@
 package com.tfandkusu.template.view.info
 
+import androidx.annotation.Keep
+
+@Keep
 sealed class InfoActivityAlias
