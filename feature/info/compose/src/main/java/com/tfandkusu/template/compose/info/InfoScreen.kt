37c84d3e9499f57e9630ef3cec27386d04889f35
Haruhiko Takada <haruhiko.takada@gmail.com>
2022-12-30 08:20:46 +0900

    Create InfoScreenPreview

--- feature/info/compose/src/main/java/com/tfandkusu/template/compose/info/InfoScreen.kt
+++ feature/info/compose/src/main/java/com/tfandkusu/template/compose/info/InfoScreen.kt
@@ -8,10 +8,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.tfandkusu.template.compose.MyTopAppBar
 import com.tfandkusu.template.compose.info.listitem.InfoListItem
 import com.tfandkusu.template.info.compose.R
+import com.tfandkusu.template.ui.theme.MyAppTheme
+import com.tfandkusu.template.viewmodel.info.InfoEffect
+import com.tfandkusu.template.viewmodel.info.InfoEvent
+import com.tfandkusu.template.viewmodel.info.InfoState
 import com.tfandkusu.template.viewmodel.info.InfoViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 @ExperimentalMaterial3Api
 @Composable
@@ -40,5 +49,30 @@ fun InfoScreen(viewModel: InfoViewModel, finish: () -> Unit = {}) {
                 }
             }
         }
+    }
+}
+
+class InfoViewModelPreview(private val previewState: InfoState) : InfoViewModel {
+
+    override fun createDefaultState() = previewState
+
+    override val state: LiveData<InfoState>
+        get() = MutableLiveData(createDefaultState())
+
+    override val effect: Flow<InfoEffect>
+        get() = flow {}
+
+    override fun event(event: InfoEvent) {
+    }
+}
+
+@ExperimentalMaterial3Api
+@Composable
+@Preview
+fun InfoScreenPreview() {
+    val state = InfoState()
+    val viewModel = InfoViewModelPreview(state)
+    MyAppTheme {
+        InfoScreen(viewModel)
     }
 }
