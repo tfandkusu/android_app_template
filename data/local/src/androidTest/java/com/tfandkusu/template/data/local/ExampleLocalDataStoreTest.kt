f351d4a7b0f54a65b5470a934919c4d063ba2a61
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-03-08 00:28:25 +0900

    Setting for instrumented tests

---
+++ data/local/src/androidTest/java/com/tfandkusu/template/data/local/ExampleLocalDataStoreTest.kt
@@ -0,0 +1,16 @@
+package com.tfandkusu.template.data.local
+
+import androidx.test.filters.SmallTest
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class ExampleLocalDataStoreTest {
+    @Test
+    fun test() {
+        assertEquals(2, 1 + 1)
+    }
+}
