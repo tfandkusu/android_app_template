ba564c126b5f3b7c91f9b28b105e46ab2ab6fef8
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-03-08 01:49:48 +0900

    Use mockk for instrumented tests.

--- data/local/src/androidTest/java/com/tfandkusu/template/data/local/ExampleLocalDataStoreTest.kt
+++ data/local/src/androidTest/java/com/tfandkusu/template/data/local/ExampleLocalDataStoreTest.kt
@@ -1,14 +1,31 @@
 package com.tfandkusu.template.data.local
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.tfandkusu.template.util.CurrentTimeGetter
 import io.kotest.matchers.shouldBe
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ExampleLocalDataStoreTest {
+    @MockK
+    private lateinit var currentTimeGetter: CurrentTimeGetter
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
     @Test
     fun test() {
-        2 shouldBe 1 + 1
+        every {
+            currentTimeGetter.get()
+        } returns 3L
+        1 + 1 shouldBe 2
+        currentTimeGetter.get() shouldBe 3L
     }
 }
