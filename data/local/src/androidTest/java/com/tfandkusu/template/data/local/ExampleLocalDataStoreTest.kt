332ad694a18820ab0acc703c99c416f4bd2241e8
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-03-08 00:42:34 +0900

    Fix using deprecated class

--- data/local/src/androidTest/java/com/tfandkusu/template/data/local/ExampleLocalDataStoreTest.kt
+++ data/local/src/androidTest/java/com/tfandkusu/template/data/local/ExampleLocalDataStoreTest.kt
@@ -1,13 +1,11 @@
 package com.tfandkusu.template.data.local
 
-import androidx.test.filters.SmallTest
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-@SmallTest
 class ExampleLocalDataStoreTest {
     @Test
     fun test() {
